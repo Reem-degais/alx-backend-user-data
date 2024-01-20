685c97bf3c61f9b1d2755676616397eecb7a484b
Reem-degais <reem.degais.33@gmail.com>
2024-01-20 21:29:18 +0000

    updated for task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -21,3 +23,22 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split("Basic ")[1].strip()
+
+    def decode_base64_authorization_header(self,
+                                               base64_authorization_header:
+                                               str) -> str:
+        """ Decodes the value of a base64 string """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded = base64.b64decode(
+                base64_authorization_header,
+                validate=True
+            )
+            return decoded.decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None
