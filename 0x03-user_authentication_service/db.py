3ddb04b18c897290f5ea48aeff8f140c4dc923a1
Reem-degais <reem.degais.33@gmail.com>
2024-01-25 22:50:17 +0000

    -

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,128 +1,80 @@
 #!/usr/bin/env python3
+"""DB module.
 """
-Flask app
-"""
-from auth import Auth
-from flask import Flask, abort, jsonify, request, redirect, url_for
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
-AUTH = Auth()
+from user import Base, User
 
 
-@app.route("/")
-def home() -> str:
-    """ Home endpoint
-        Return:
-            - Logout message JSON represented
+class DB:
+    """DB class.
     """
-    return jsonify({"message": "Bienvenue"})
 
+    def __init__(self) -> None:
+        """Initialize a new DB instance.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
 
-@app.route("/sessions", methods=["POST"])
-def login():
-    """ Login endpoint
-        Form fields:
-            - email
-            - password
-        Return:
-            - user email and login message JSON represented
-            - 401 if credential are invalid
-    """
-    email = request.form.get("email")
-    password = request.form.get("password")
-    if not AUTH.valid_login(email, password):
-        abort(401)
-    session_id = AUTH.create_session(email)
-    response = jsonify({"email": email, "message": "logged in"})
-    response.set_cookie("session_id", session_id)
-    return response
+    @property
+    def _session(self) -> Session:
+        """Memoized session object.
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
 
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user to the database.
+        """
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
+        return new_user
 
-@app.route("/sessions", methods=["DELETE"])
-def logout():
-    """ Logout endpoint
-        Return:
-            - redirect to home page
-    """
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if not user:
-        abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect(url_for("home"))
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user based on a set of filters.
+        """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
 
-
-@app.route("/users", methods=["POST"])
-def users():
-    """ New user signup endpoint
-        Form fields:
-            - email
-            - password
-    """
-    email = request.form.get("email")
-    password = request.form.get("password")
-    try:
-        AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
-
-
-@app.route("/profile")
-def profile() -> str:
-    """ User profile endpoint
-        Return:
-            - user email JSON represented
-            - 403 if session_id is not linked to any user
-    """
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if not user:
-        abort(403)
-    return jsonify({"email": user.email})
-
-
-@app.route("/reset_password", methods=["POST"])
-def get_reset_password_token() -> str:
-    """ Reset password token endpoint
-        Form fields:
-            - email
-        Return:
-            - email and reset token JSON represented
-            - 403 if email is not associated with any user
-    """
-    email = request.form.get("email")
-    try:
-        reset_token = AUTH.get_reset_password_token(email)
-    except ValueError:
-        abort(403)
-
-    return jsonify({"email": email, "reset_token": reset_token})
-
-
-@app.route("/reset_password", methods=["PUT"])
-def update_password():
-    """ Password update endpoint
-        Form fields:
-            - email
-            - reset_token
-            - new_password
-        Return:
-            - user email and password update message JSON represented
-            - 403 if reset token is not provided or not linked to any user
-    """
-    email = request.form.get("email")
-    new_password = request.form.get("new_password")
-    reset_token = request.form.get("reset_token")
-
-    try:
-        AUTH.update_password(reset_token, new_password)
-    except ValueError:
-        abort(403)
-    return jsonify({"email": email, "message": "Password updated"})
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user based on a given id.
+        """
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_source = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_source[getattr(User, key)] = value
+            else:
+                raise ValueError()
+        self._session.query(User).filter(User.id == user_id).update(
+            update_source,
+            synchronize_session=False,
+        )
+        self._session.commit()
