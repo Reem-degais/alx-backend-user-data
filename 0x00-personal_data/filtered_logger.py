3da9ccb39021077907c25d1a656af59a7a7c41e1
Reem-degais <reem.degais.33@gmail.com>
2024-02-28 23:24:08 +0000

    update for task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,6 +22,7 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                          field+'='+redaction+separator, message)
     return message
 
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -33,6 +34,7 @@ class RedactingFormatter(logging.Formatter):
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
+
     def format(self, record: logging.LogRecord) -> str:
         """
          filter values in incoming log records using filter_datum"""
