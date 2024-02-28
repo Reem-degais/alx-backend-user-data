c89dfb38f59a7385ecf257a580649b3f12ec030e
Reem-degais <reem.degais.33@gmail.com>
2024-02-28 22:01:57 +0000

    create README.md and filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+ function called filter_datum that returns the log message
+"""
+
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Return an obfuscated log message.
+    arg:
+    fields: a list of strings representing all fields to obfuscate
+    redaction: a string representing by what the field will be obfuscated
+    message: a string representing the log line
+    separator: a string representing by which character is separating all 
+    fields in the log line (message)
+    """
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return message
