ea27542e2b9b46e385449b7b29ed3d703bc40534
Reem-degais <reem.degais.33@gmail.com>
2024-02-28 22:07:16 +0000

    create README.md and filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,9 +14,7 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     arg:
     fields: a list of strings representing all fields to obfuscate
     redaction: a string representing by what the field will be obfuscated
-    message: a string representing the log line
-    separator: a string representing by which character is separating all 
-    fields in the log line (message)
+    message: a string representing the log line separator
     """
     for field in fields:
         message = re.sub(field+'=.*?'+separator,
